048341bf360e04299ae211ca2707ad75aa8a4fdb
urbanst0 <167612420+urbanst0@users.noreply.github.com>
2024-05-13 07:48:56 -0600

    Add files via upload

---
+++ presentacion/JS/script.js
@@ -0,0 +1,9 @@
+function mostrarModal(idModal) {
+    var modal = document.getElementById(idModal);
+    modal.style.display = "block";
+}
+
+function cerrarModal(idModal) {
+    var modal = document.getElementById(idModal);
+    modal.style.display = "none";
+}
